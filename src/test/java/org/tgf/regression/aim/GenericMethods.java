34ba62936cf34777b3df652f789efc3f4d39a0dd
Nazareena Sulthana Dasthagir khader sheriff <nsulthan@in.ibm.com>
2018-04-06 12:47:59 +0530

    changes committed

--- src/test/java/org/tgf/regression/aim/GenericMethods.java
+++ src/test/java/org/tgf/regression/aim/GenericMethods.java
@@ -439,11 +439,11 @@ public class GenericMethods extends TestData
 		File source = ts.getScreenshotAs(OutputType.FILE);
 		// after execution, you could see a folder "FailedTestsScreenshots"
 		// under src folder
-		/*String destination = System.getProperty("user.dir")
+		String destination = System.getProperty("user.dir")
 				+ "/FailedTestsScreenshots/" + screenshotName + dateName
-				+ ".png";*/
-		String destination = "./FailedTestsScreenshots/" + screenshotName + dateName
 				+ ".png";
+		/*String destination = "../FailedTestsScreenshots/" + screenshotName + dateName
+				+ ".png";*/
 				
 		File finalDestination = new File(destination);
 		FileUtils.copyFile(source, finalDestination);
